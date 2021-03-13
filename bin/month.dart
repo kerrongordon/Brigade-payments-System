e2cd2a01c5610e3dfc784b1181440d660561efc3
kerrongordon <kgpsounds.com@gmail.com>
2021-03-13 14:10:00 -0400

    better month and year input

--- bin/month.dart
+++ bin/month.dart
@@ -3,17 +3,55 @@ import 'dart:io';
 import 'package:intl/intl.dart';
 
 import 'core/month-weekday.dart';
+import 'inputs/data-input.dart';
 import 'models/month.model.dart';
 import 'utils/clearscreen-utils.dart';
 
 Month month() {
   final month = Month();
-  print('Enther Month Number EXAMPLE ( 1 ) for January');
-  month.month = int.parse(stdin.readLineSync());
-  print('');
-  print('Enther year');
-  month.year = int.parse(stdin.readLineSync());
-  print('');
+  final dateTime = DateTime.now();
+  final thisMonth = DateFormat('MMMM', 'en_US').format(dateTime);
+
+  while (month.month == null) {
+    final isThisMonth =
+        dataInput(message: 'Payment For The Month Of $thisMonth (Y)es or (N)o')
+            .toLowerCase();
+
+    if (isThisMonth == 'yes' || isThisMonth == 'y') {
+      month.month = dateTime.month;
+      print('Payments for $thisMonth');
+      print('');
+    } else if (isThisMonth == 'no' || isThisMonth == 'n') {
+      print('Enther Month Number EXAMPLE ( 1 ) for January');
+      month.month = int.parse(stdin.readLineSync());
+      print(
+          'Payments for ${DateFormat('MMMM').format(DateTime(dateTime.year, month.month))}');
+      print('');
+    } else {
+      clearscreen();
+      month.month = null;
+    }
+  }
+
+  while (month.year == null) {
+    final isthisYear = dataInput(
+            message: 'Payment For The Year ${dateTime.year} (Y)es or (N)o')
+        .toLowerCase();
+
+    if (isthisYear == 'yes' || isthisYear == 'y') {
+      month.year = dateTime.year;
+      print('Payments for $thisMonth ${dateTime.year}');
+      print('');
+    } else if (isthisYear == 'no' || isthisYear == 'n') {
+      print('Enther year');
+      month.year = int.parse(stdin.readLineSync());
+      print('Payments for $thisMonth ${month.year}');
+      print('');
+    } else {
+      clearscreen();
+      month.year = null;
+    }
+  }
 
   month.month_name =
       DateFormat('MMMM').format(DateTime(month.year, month.month));
