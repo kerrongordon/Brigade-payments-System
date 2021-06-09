8ec9edf7c6eedfb551b16c76171e9613d0723bdd
kerrongordon <kgpsounds.com@gmail.com>
2021-06-09 06:46:16 -0400

    better week day

--- bin/core/month-weekday.dart
+++ bin/core/month-weekday.dart
@@ -1,19 +1,25 @@
-int totalWeekDayOfMonth({int year, int month, int day = 1}) {
-  final totalDayOfMonth = totalDaysOfMonth(year, month);
-  var result = 0;
-  var tempDateTime = DateTime(year, month, day);
-  for (var i = day; i <= totalDayOfMonth; i++) {
-    tempDateTime = DateTime(tempDateTime.year, tempDateTime.month, i);
-    if (tempDateTime.weekday == DateTime.saturday ||
-        tempDateTime.weekday == DateTime.sunday) {
-    } else {
-      result++;
+int totalWeekDayOfMonth({int year, int month, int firstday = 1}) {
+  final totalDays = _totalDaysOfMonth(month, year);
+
+  final startDate = DateTime(year, month, firstday);
+  final endDate = DateTime(year, month, totalDays);
+
+  final nbDays = endDate.difference(startDate).inDays + 1;
+
+  final days = List<int>.generate(nbDays, (index) {
+    final weekDay =
+        DateTime(startDate.year, startDate.month, startDate.day + (index))
+            .weekday;
+    if (weekDay != DateTime.saturday && weekDay != DateTime.sunday) {
+      return 1;
     }
-  }
-  return result;
+    return 0;
+  });
+
+  return days.reduce((a, b) => a + b);
 }
 
-int totalDaysOfMonth(int month, int year) {
+int _totalDaysOfMonth(int month, int year) {
   final result =
       (month < 12) ? DateTime(year, month + 1, 0) : DateTime(year + 1, 1, 0);
   return result.day;
